a51ac6544af9c7067a530955d2b2be48bfcc4777
Rami Dabit <55854874+ramidabit@users.noreply.github.com>
2021-03-12 05:36:39 -0800

    Add dual channel

--- main.c
+++ main.c
@@ -25,12 +25,16 @@ typedef struct {
 
 L3GD20_Data_t currentSpeed;
 
-uint32_t volatile currentValue = 0;
-uint32_t volatile lastValue = 0;
-uint32_t volatile overflowCount = 0;
-uint32_t volatile timeInterval = 0;
+uint32_t volatile currentValue_1 = 0;
+uint32_t volatile currentValue_2 = 0;
+uint32_t volatile lastValue_1 = 0;
+uint32_t volatile lastValue_2 = 0;
+uint32_t volatile overflowCount_1 = 0;
+uint32_t volatile overflowCount_2 = 0;
+uint32_t volatile timeInterval_1 = 0;
+uint32_t volatile timeInterval_2 = 0;
 
-void Trigger_Setup() {
+void Ch1_Trigger_Setup() {
 	// Enable GPIO port E clock
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN;
 	// Configure PE11 as alternative function (10)
@@ -39,7 +43,7 @@ void Trigger_Setup() {
 	// Configure and select alternative function for PE11 (AF1)
 	GPIOE->AFR[1] &= ~GPIO_AFRL_AFRL3;
 	GPIOE->AFR[1] |= (0x1 << 3*4);
-	// Configure PE8 as no pull-up, no pull-down (00)
+	// Configure PE11 as no pull-up, no pull-down (00)
 	GPIOE->PUPDR &= ~GPIO_PUPDR_PUPD11;
 	// Configure PE11 output type as push-pull (0)
 	GPIOE->OTYPER &= ~GPIO_OTYPER_OT11;
@@ -92,7 +96,71 @@ void Trigger_Setup() {
 	TIM1->CR1 |= 0x1;
 }
 
-void Input_Capture_Setup() {
+void Ch2_Trigger_Setup() {
+	// Please note: Many steps are repeated in case Ch2 is used on its own
+
+	// Enable GPIO port E clock
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN;
+	// Configure PE13 as alternative function (10)
+	GPIOE->MODER &= ~GPIO_MODER_MODE13;
+	GPIOE->MODER |= GPIO_MODER_MODE13_1;
+	// Configure and select alternative function for PE13 (AF1)
+	GPIOE->AFR[1] &= ~GPIO_AFRL_AFRL5;
+	GPIOE->AFR[1] |= (0x1 << 5*4);
+	// Configure PE13 as no pull-up, no pull-down (00)
+	GPIOE->PUPDR &= ~GPIO_PUPDR_PUPD13;
+	// Configure PE13 output type as push-pull (0)
+	GPIOE->OTYPER &= ~GPIO_OTYPER_OT13;
+	// Configure PE13 output speed as very high (11)
+	GPIOE->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR13;
+
+	// Enable TIM1 clock
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+	// Set prescaler value to 15
+	TIM1->PSC &= ~TIM_PSC_PSC;
+	TIM1->PSC |= 0xF;
+
+	// Enable preload for TIM1 CH 3
+	TIM1->CR1 |= TIM_CR1_ARPE;
+	// Set auto-reload value to 20
+	TIM1->ARR &= ~TIM_ARR_ARR;
+	TIM1->ARR |= 0x14;
+
+	// Set capture and compare value to 10
+	TIM1->CCR3 &= ~TIM_CCR3_CCR3;
+	TIM1->CCR3 |= 0xA;
+
+	// Configure channel to be used in output compare mode
+	// Clear the output compare mode bits for channel 3
+	TIM1->CCMR2 &= ~TIM_CCMR2_OC3M;
+	// Set output control mode bits to PWM mode 1
+	TIM1->CCMR2 |= TIM_CCMR2_OC3M_1;
+	TIM1->CCMR2 |= TIM_CCMR2_OC3M_2;
+	// Enable output compare preload
+	TIM1->CCMR2 |= TIM_CCMR2_OC3PE;
+
+	// Enable channel 3 output
+	TIM1->CCER |= TIM_CCER_CC3E;
+
+	// Enable main output and off-state selection
+	TIM1->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR;
+
+	// Enable update generation
+	TIM1->EGR |= 0x1;
+
+	// Enable update interrupt for timer 4
+	TIM1->DIER |= TIM_DIER_UIE;
+	// Clear update interrupt flag
+	TIM1->SR &= ~TIM_SR_UIF;
+
+	// Set direction such that timer counts up
+	TIM1->CR1 &= ~TIM_CR1_DIR;
+	// Enable the counter
+	TIM1->CR1 |= 0x1;
+}
+
+void Ch1_Input_Capture_Setup() {
 	// Enable GPIO port B clock
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 	// Configure PB6 as alternative function (10)
@@ -151,6 +219,67 @@ void Input_Capture_Setup() {
 	NVIC_SetPriority(TIM4_IRQn, 2);
 }
 
+void Ch2_Input_Capture_Setup() {
+	// Please note: Many steps are repeated in case Ch2 is used on its own
+
+	// Enable GPIO port B clock
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	// Configure PB7 as alternative function (10)
+	GPIOB->MODER &= ~GPIO_MODER_MODE7;
+	GPIOB->MODER |= GPIO_MODER_MODE7_1;
+	// Configure and select alternative function for PB7 (AF2)
+	GPIOB->AFR[0] &= ~GPIO_AFRL_AFRL7;
+	GPIOB->AFR[0] |= (0x2 << 7*4);
+	// Configure PB7 as no pull-up, no pull-down (00)
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD7;
+
+	// Enable TIM4 clock
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM4EN;
+
+	// Set prescaler value to 15
+	TIM4->PSC &= ~TIM_PSC_PSC;
+	TIM4->PSC |= 0xF;
+
+	// Enable preload for TIM4 CH 2
+	TIM4->CR1 |= TIM_CR1_ARPE;
+	// Set auto-reload value to max
+	TIM4->ARR |= TIM_ARR_ARR;
+
+	// Configure channel to be used in input capture mode
+	// Clear capture and compare mode register
+	TIM4->CCMR1 &= ~TIM_CCMR1_CC2S;
+	// Set to capture input on timer channel 2
+	TIM4->CCMR1 |= TIM_CCMR1_CC2S_0;
+
+	// Enable input capture on both rising and falling edges
+	TIM4->CCER |= TIM_CCER_CC2E;
+	TIM4->CCER |= TIM_CCER_CC2P;
+	TIM4->CCER |= TIM_CCER_CC2NP;
+
+	// Enable interrupts for timer 4
+	TIM4->DIER |= TIM_DIER_CC2IE;
+	// Enable DMA requests for timer 4
+	TIM4->DIER |= TIM_DIER_CC2DE;
+	// Enable update interrupt for timer 4
+	TIM4->DIER |= TIM_DIER_UIE;
+
+	// Enable update generation
+	TIM4->EGR |= 0x1;
+
+	// Clear update interrupt flag
+	TIM4->SR &= ~TIM_SR_UIF;
+
+	// Set direction such that timer counts up
+	TIM4->CR1 &= ~TIM_CR1_DIR;
+
+	// Enable the counter
+	TIM4->CR1 |= 0x1;
+
+	// Enable TIM4_IRQn interrupt in NVIC and set priority to 2
+	NVIC_EnableIRQ(TIM4_IRQn);
+	NVIC_SetPriority(TIM4_IRQn, 2);
+}
+
 void TIM4_IRQHandler(void) {
 	// currentValue = CCR_New
 	// lastValue = CCR_Last
@@ -160,20 +289,34 @@ void TIM4_IRQHandler(void) {
 
 	if(TIM4->SR & TIM_SR_UIF) { // UIF Interrupt
 		TIM4->SR &= ~TIM_SR_UIF;
-		overflowCount++;
+		overflowCount_1++;
+		overflowCount_2++;
 
 	} else if(TIM4->SR & TIM_SR_CC1IF) { // Channel 1
 		TIM4->SR &= ~TIM_SR_CC1IF;
 
 		if(GPIOB->IDR & GPIO_IDR_ID6) { // Echo pin high (rising edge)
-			lastValue = TIM4->CCR1 & TIM_CCR1_CCR1;
+			lastValue_1 = TIM4->CCR1 & TIM_CCR1_CCR1;
 			
 		} else { // Echo pin low (falling edge)
-			currentValue = TIM4->CCR1 & TIM_CCR1_CCR1;
+			currentValue_1 = TIM4->CCR1 & TIM_CCR1_CCR1;
 
 			// Compute pulse width and reset overflow count
-			timeInterval = currentValue - lastValue + 65536*overflowCount;
-			overflowCount = 0;
+			timeInterval_1 = currentValue_1 - lastValue_1 + 65536*overflowCount_1;
+			overflowCount_1 = 0;
+		}
+	} else if(TIM4->SR & TIM_SR_CC2IF) { // Channel 2
+		TIM4->SR &= ~TIM_SR_CC2IF;
+
+		if(GPIOB->IDR & GPIO_IDR_ID7) { // Echo pin high (rising edge)
+			lastValue_2 = TIM4->CCR2 & TIM_CCR2_CCR2;
+			
+		} else { // Echo pin low (falling edge)
+			currentValue_2 = TIM4->CCR2 & TIM_CCR2_CCR2;
+
+			// Compute pulse width and reset overflow count
+			timeInterval_2 = currentValue_2 - lastValue_2 + 65536*overflowCount_2;
+			overflowCount_2 = 0;
 		}
 	}
 }
@@ -236,10 +379,12 @@ int main(void){
 	SysTick_Init();
 
 	// Input Capture Setup
-	Input_Capture_Setup();
+	Ch1_Input_Capture_Setup();
+	Ch2_Input_Capture_Setup();
 	
 	// Trigger Setup
-	Trigger_Setup();
+	Ch1_Trigger_Setup();
+	Ch2_Trigger_Setup();
 
 	// LCD Setup
 	LCD_Initialization();
@@ -271,9 +416,10 @@ int main(void){
 
 		if(active) {
 			// The HC-SR04 sensor can measure distances between 2cm and 400cm
-			if(timeInterval <= 58*400 && timeInterval >= 58*2) {
+			if((timeInterval_1 <= 58*400 && timeInterval_1 >= 58*2) ||
+			   (timeInterval_2 <= 58*400 && timeInterval_2 >= 58*2)) {
 				// Convert sensor measurements to a distance in cm
-				sprintf(message, "%6d", timeInterval / 58);
+				sprintf(message, "%3d%3d", timeInterval_1/58, timeInterval_2/58);
 			} else {
 				// No object in range
 				sprintf(message, "%s", "------");
